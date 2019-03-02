9f3f919e76ccdb8f25846a7200ffbd0b7850a947
Julien LE THENO <julien.letheno@gmail.com>
2019-03-02 14:24:07 +0100

    [Doc][Style] regex function doc and ifdefs protect

--- src/regex.h
+++ src/regex.h
@@ -1,1 +1,11 @@
-int regex(char*, char*);
+#ifndef REGEX_H
+#define REGEX_H
+
+/** tests a regex against an input string
+ *  @param input : the input string to test
+ *  @param regex : the regex used to test the input string
+ *  @return 1 if the input is okay, 0 if it isnt.
+ */
+int regex(char* input, char* regex);
+
+#endif
