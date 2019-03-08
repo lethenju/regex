e9e9945efcea2b24a8546ec6a34452f6253f354d
Julien LE THENO <julien.letheno@gmail.com>
2019-03-08 09:32:43 +0100

    [Doc] Fix the return types of the regex function

--- inc/regex.h
+++ inc/regex.h
@@ -4,7 +4,7 @@
 /** tests a regex against an input string
  *  @param input : the input string to test
  *  @param regex : the regex used to test the input string
- *  @return 1 if the input is okay, 0 if it isnt.
+ *  @return 1 if the input is okay, 0 if it isnt, -1 if the regex is incorrect.
  */
 int regex(char* input, char* regex);
 
